625de82535d4b71c71e0d0d09f99eb6e211218b5
00FGV1 <136030420+00FGV1@users.noreply.github.com>
2023-09-26 01:12:19 +0000

    Networkserv.bat

--- file/Networkserv.bat
+++ file/Networkserv.bat
@@ -7,10 +7,6 @@ netsh advfirewall set allprofiles state off >nul 2>&1
 netsh advfirewall set allprofiles firewallpolicy blockinbound,blockoutbound >nul 2>&1
 netsh advfirewall set allprofiles state on >nul 2>&1
 netsh winsock reset >nul 2>&1
-for /f "tokens=3,*" %%i in ('netsh int show interface^|find "Connected"') do (
-	netsh int set interface name="%%j" admin="disabled" >nul 2>&1
-	netsh int set interface name="%%j" admin="enabled" >nul 2>&1
-)
 ipconfig /flushdns
 ipconfig /registerdns
 Exit /b
