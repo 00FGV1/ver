dae42de6f923f76cf8e497785eeee96a57016d37
00FGV1 <136030420+00FGV1@users.noreply.github.com>
2023-09-26 01:25:05 +0000

    Networkserv.bat

--- file/Networkserv.bat
+++ file/Networkserv.bat
@@ -3,9 +3,6 @@ title Network
 cls
 netsh winsock reset
 netsh int tcp set global autotuninglevel=normal
-netsh advfirewall set allprofiles state off >nul 2>&1
-netsh advfirewall set allprofiles firewallpolicy blockinbound,blockoutbound >nul 2>&1
-netsh advfirewall set allprofiles state on >nul 2>&1
 netsh winsock reset >nul 2>&1
 ipconfig /flushdns
 ipconfig /registerdns
