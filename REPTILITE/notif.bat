978f95210ffd1421c5e6b8e7859118676ced7e6f
00FGV1 <136030420+00FGV1@users.noreply.github.com>
2023-10-31 02:21:05 +0100

    Add files via upload

---
+++ REPTILITE/notif.bat
@@ -0,0 +1,1 @@
+start /b powershell [Reflection.Assembly]::LoadWithPartialName("""System.Windows.Forms""");$obj=New-Object Windows.Forms.NotifyIcon;$obj.Icon = [drawing.icon]::ExtractAssociatedIcon("""6754.ico""");$obj.Visible = $True;$obj.ShowBalloonTip(100000, """Optimisation""","""Fin de l'optimisation""",1)>nul
