4d333f4619578526d04fd8baf046d74b538b76d2
00FGV1 <136030420+00FGV1@users.noreply.github.com>
2023-10-31 02:28:30 +0100

    Update notif.bat

--- REPTILITE/notif.bat
+++ REPTILITE/notif.bat
@@ -1,1 +1,4 @@
+@echo off
+cls
 start /b powershell [Reflection.Assembly]::LoadWithPartialName("""System.Windows.Forms""");$obj=New-Object Windows.Forms.NotifyIcon;$obj.Icon = [drawing.icon]::ExtractAssociatedIcon("""6754.ico""");$obj.Visible = $True;$obj.ShowBalloonTip(100000, """Optimisation""","""Fin de l'optimisation""",1)>nul
+Exit
